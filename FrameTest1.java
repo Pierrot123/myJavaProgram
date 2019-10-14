36917e650deeeef5b48768067e841fca96d99057
Pierrot123 <pak9166@naver.com>
2019-10-14 17:00:32 +0900

    업데이트 doc

---
+++ FrameTest1.java
@@ -0,0 +1,14 @@
+import java.awt.Frame;
+
+public class FrameTest1
+{
+
+    public static void main(String[] args)
+    {
+        Frame f=new Frame();
+        f.setTitle("첫 번째 프레임 입니다.");
+        f.setBounds(500, 500, 300, 300);
+        f.setVisible(true);
+        
+    }
+}
